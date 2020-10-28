c8fc95d255f25c7be2614196d7ba579ec8925fef
ricardolp <ricardo.lopez@interware.com.mx>
2020-10-28 11:35:19 -0600

    Se agregan nuevos metodos para sus pruebas unitarias

--- src/main/java/service/ProducerService.java
+++ src/main/java/service/ProducerService.java
@@ -1,6 +1,12 @@
 package service;
 
+import java.util.List;
+
 public interface ProducerService {
     boolean deliver(Message message);
     String cambiaMensaje(Message message);
+    List<String> tiposDocumentos();
+    String generadorTurno();
+    String encuentraTramite(String tramite);
+    boolean agregaTramite(String tramite);
 }
--- src/main/java/service/ProducerServiceImpl.java
+++ src/main/java/service/ProducerServiceImpl.java
@@ -1,5 +1,8 @@
 package service;
 
+import java.time.LocalDateTime;
+import java.util.*;
+
 public class ProducerServiceImpl implements ProducerService{
 
     Validador validador;
@@ -21,4 +24,37 @@ public class ProducerServiceImpl implements ProducerService{
         String nuevoMensaje = "Mensaje: " + message.getContenido();
         return nuevoMensaje;
     }
+
+    @Override
+    public List<String> tiposDocumentos() {
+        List<String> tiposDocumentos = new ArrayList<>();
+        tiposDocumentos.add("comprobante de domicilio");
+        tiposDocumentos.add("identificacion oficial");
+        return tiposDocumentos;
+    }
+
+    @Override
+    public String generadorTurno() {
+        LocalDateTime now = LocalDateTime.now();
+        String turno = "NAY" + now.toString();
+        return turno;
+    }
+
+    @Override
+    public String encuentraTramite(String tramite) {
+        Map<String, String> tramites = new HashMap<>();
+        tramites.put("ALTA","Alta vehiculo");
+        tramites.put("BAJA", "Baja vehiculo");
+
+        String respuesta = tramites.get(tramite);
+        return respuesta;
+    }
+
+    @Override
+    public boolean agregaTramite(String tramite) {
+        Set<String> tramites = new HashSet<>();
+        boolean agregado = tramites.add(tramite);
+        return agregado;
+    }
+
 }
