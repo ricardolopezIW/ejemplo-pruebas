cd1d29efcd357cc1168364e6091825e48e811687
ricardolp <ricardo.lopez@interware.com.mx>
2020-10-29 11:44:21 -0600

    Se envia interfaz con fecha parametrizada

--- src/main/java/service/ProducerService.java
+++ src/main/java/service/ProducerService.java
@@ -1,12 +1,13 @@
 package service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ProducerService {
     boolean deliver(Message message);
     String cambiaMensaje(Message message);
     List<String> tiposDocumentos();
-    String generadorTurno();
+    String generadorTurno(LocalDateTime fecha);
     String encuentraTramite(String tramite);
     boolean agregaTramite(String tramite);
 }
